f1c71a02fd86fc7ab50e35eea8603c6314e532cd
Eric Bower <me@erock.io>
2022-05-16 17:25:51 -0400

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
 
 chmod 600 __TEMP_INPUT_KEY_FILE
 
-scp -O -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_SRC} "${INPUT_USER}"@"${INPUT_HOST}":/
+scp -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_SRC} "${INPUT_USER}"@"${INPUT_HOST}":/
